daed091383a15060658c995a53500c8e5fc8d213
rt0 <rajm.toth@gmail.com>
2021-05-19 14:42:21 +0200

    Implemented the Bellman-Ford algorithm.

---
+++ graph/Bellman-Ford/README.md
@@ -0,0 +1,14 @@
+# Bellman-Ford Algorithm
+
+<h2>The Bellman-Ford Algorithm is used for calculating shortest path in graphs, which migh have negative weights</h2>
+<h3>Time complexity:</h3>
+<ul>
+    <li>O(VE)</li>
+    <li>Θ(VE)</li>
+    <li>Ω(E)</li>
+</ul>
+<h3>Space complexity:</h3>
+<ul>
+    <li>O(V)</li>
+</ul>
+<p>Where V represents the vertices and E represents the edges.</p>
---
+++ graph/Bellman-Ford/main.py
@@ -0,0 +1,53 @@
+class Graph:
+
+    def __init__(self, v: int, s: int) -> None:
+        
+        self.num_vertices = v
+        self.g = []
+        self.source = s
+
+    def add_edge(self, m: int, n: int, w: int):
+
+        self.g.append([m, n, w])
+
+    def BellmanFord(self):
+
+        inf = float('Inf')
+
+        distances = [inf] * self.num_vertices # Init all distances as infinite.
+
+        distances[self.source] = 0 # Set source distance to null.
+
+        for _ in range(self.num_vertices - 1): # relax edges |V| - 1 times.
+
+            for m, n, w in self.g: # Iterate over each edge, vertex and weight.
+
+                if distances[m] != inf and distances[m] + w < distances[n]:
+                    
+                    distances[n] = distances[m] + w
+
+        # Final loop to validate that no negative cycles exist in the graph.
+        for m, n, w in self.g:
+
+            if distances[m] != inf and distances[m] + w < distances[n]:
+                return None
+
+
+        return distances
+
+
+if __name__ == '__main__':
+
+    g = Graph(5, 0)
+    
+    g.add_edge(0, 1, -1) 
+    g.add_edge(0, 2, 4) 
+    g.add_edge(1, 2, 3) 
+    g.add_edge(1, 3, 2) 
+    g.add_edge(1, 4, 2) 
+    g.add_edge(3, 2, 5) 
+    g.add_edge(3, 1, 1) 
+    g.add_edge(4, 3, -3)
+
+    res = g.BellmanFord()
+    print(res)
